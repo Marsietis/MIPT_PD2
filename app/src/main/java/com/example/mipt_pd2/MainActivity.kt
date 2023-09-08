966d635828af59e95cbec4a61fbdebe931730f8c
Marsietis <martynas.matijosius@gmail.com>
2023-09-08 22:27:47 +0300

    Added option to the spinner element

--- app/src/main/java/com/example/mipt_pd2/MainActivity.kt
+++ app/src/main/java/com/example/mipt_pd2/MainActivity.kt
@@ -2,10 +2,18 @@ package com.example.mipt_pd2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Set options for the spinner
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        val adapter = ArrayAdapter.createFromResource(this, R.array.spinner_options, android.R.layout.simple_spinner_item)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner.adapter = adapter
     }
 }
---
+++ app/src/main/java/com/example/mipt_pd2/WordCounter.kt
@@ -0,0 +1,5 @@
+package com.example.mipt_pd2
+
+class WordCounter {
+
+}
