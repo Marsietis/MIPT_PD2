65bf90dd38da29bc636849b2bf2dbd8c2f889773
Marsietis <martynas.matijosius@gmail.com>
2023-09-09 11:20:17 +0300

    Added basic logic for counting words on button click

--- app/src/main/java/com/example/mipt_pd2/MainActivity.kt
+++ app/src/main/java/com/example/mipt_pd2/MainActivity.kt
@@ -3,7 +3,10 @@ package com.example.mipt_pd2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.EditText
 import android.widget.Spinner
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,8 +15,22 @@ class MainActivity : AppCompatActivity() {
 
         // Set options for the spinner
         val spinner = findViewById<Spinner>(R.id.spinner)
-        val adapter = ArrayAdapter.createFromResource(this, R.array.spinner_options, android.R.layout.simple_spinner_item)
+        val adapter = ArrayAdapter.createFromResource(
+            this,
+            R.array.spinner_options,
+            android.R.layout.simple_spinner_item
+        )
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
+
+        val button = findViewById<Button>(R.id.button)
+        val resultText = findViewById<TextView>(R.id.textView2)
+
+        button.setOnClickListener {
+            val text = findViewById<EditText>(R.id.editTextTextMultiLine).text.toString()
+            val wordCounter = WordCounter()
+            val result = wordCounter.countWords(text)
+            resultText.text = result.toString()
+        }
     }
 }
--- app/src/main/java/com/example/mipt_pd2/WordCounter.kt
+++ app/src/main/java/com/example/mipt_pd2/WordCounter.kt
@@ -1,5 +1,7 @@
 package com.example.mipt_pd2
 
 class WordCounter {
-
+    fun countWords(text: String): Int {
+        return text.split(" ").size
+    }
 }
