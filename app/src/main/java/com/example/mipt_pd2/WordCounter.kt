0cb526fbfdd6140dade3129b57f6f2e5e9fc98bf
Marsietis <martynas.matijosius@gmail.com>
2023-09-10 13:30:43 +0300

    Updated logic for counting words and added a function to count symbols

--- app/src/main/java/com/example/mipt_pd2/WordCounter.kt
+++ app/src/main/java/com/example/mipt_pd2/WordCounter.kt
@@ -2,6 +2,12 @@ package com.example.mipt_pd2
 
 class WordCounter {
     fun countWords(text: String): Int {
-        return text.split(" ").size
+        val regex = "\\b\\w+\\b".toRegex()
+        return regex.findAll(text).count()
+    }
+
+    fun countSymbols(text: String): Int {
+        val punctuationChars = setOf('.', ',', '!', '?', ';', ':')
+        return text.count { char -> char in punctuationChars }
     }
 }
